d12add535d7a46bba8e828a6a70ef163fda2f5a9
Mayank <mayankdahiya8431@gmail.com>
2025-07-04 19:03:25 +0530

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score = "mayank"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 false => 0
+
+let isLoggedIn = "mayank"
+let booleanNumber = Boolean(isLoggedIn)
+console.log(booleanNumber) // true
+
+//"" => false
+//"mayank" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber) // "33"
+console.log(typeof stringNumber) // string
